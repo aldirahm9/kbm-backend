a9db6b7c01947949a31c3ba7b4f7c0b4134e41bb
Aldi Rahmansyah <aldi.rahmansyah99@gmail.com>
2021-03-25 12:18:24 +0700

    Configuring

--- config/cors.php
+++ config/cors.php
@@ -4,31 +4,36 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
+    | Laravel CORS Options
     |--------------------------------------------------------------------------
     |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
+    | The allowed_methods and allowed_headers options are case-insensitive.
     |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    | You don't need to provide both allowed_origins and allowed_origins_patterns.
+    | If one of the strings passed matches, it is considered a valid origin.
+    |
+    | If ['*'] is provided to allowed_methods, allowed_origins or allowed_headers
+    | all methods / origins / headers are allowed.
     |
     */
 
+    /*
+     * You can enable CORS for 1 or multiple paths.
+     * Example: ['api/*']
+     */
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
 
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'],
 
-    'exposed_headers' => false,
+    'exposed_headers' => [],
 
-    'max_age' => false,
+    'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\AbsenResource;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::group(['middleware' => 'api','prefix'=> 'auth'], function () {
     Route::post('login', 'AuthController@login');
@@ -24,3 +26,15 @@ Route::group(['middleware' => 'api','prefix'=> 'auth'], function () {
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 });
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('kelas', 'HomeController@getAllKelas');
+    Route::post('kelas/{id}', 'KelasController@getKelas');
+    Route::post('kelas/{id}/form-05', 'KelasController@getForm05');
+    Route::post('kelas/{id}/form-05/add', 'KelasController@buatPertemuan');
+    Route::post('form-05/{id}/valid', 'KelasController@validPertemuan');
+    Route::post('form-05/{id}/buka-absen', 'KelasController@bukaAbsen');
+    Route::post('kelas/{id}/form-06', 'KelasController@getForm06');
+    Route::post('kelas/{id}/form-06-nilai', 'KelasController@getForm06Nilai');
+});
+
